08710aab002f0d1f1552f5b85ae4139cdb1d4141
Sydney Pogue <spogue1441@gmail.com>
2023-06-03 09:58:17 -1000

    Modify: Modified database so that it can access local data

--- database.js
+++ database.js
@@ -3,13 +3,7 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  protocol: 'postgres'
 });
 
 module.exports = sequelize;
