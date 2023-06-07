97433e6005b787185ff7c7b82d265ae6bf016cdb
Sydney Pogue <spogue1441@gmail.com>
2023-06-06 17:27:05 -1000

    add: added models within database.js and linked database to app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const db = require('./database');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- database.js
+++ database.js
@@ -1,4 +1,26 @@
-
+const Sequelize = require('sequelize');
 const dbConfig = require('./config.json');
 
-module.exports = dbConfig;
+const sequelize = new Sequelize(
+  dbConfig.development.database,
+  dbConfig.development.username,
+  dbConfig.development.password,
+  {
+    host: dbConfig.development.host,
+    dialect: dbConfig.development.dialect,
+    logging: false, // turning off logging for cleaner console output
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Models/tables
+db.users = require('./models/user.js')(sequelize, Sequelize);
+db.rings = require('./models/ring.js')(sequelize, Sequelize);
+db.bands = require('./models/band.js')(sequelize, Sequelize);
+db.gems = require('./models/gem.js')(sequelize, Sequelize);
+
+module.exports = db;
